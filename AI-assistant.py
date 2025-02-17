a96a56cc9cb4e7934fd3790be7759599443a16fc
gio19 <gio_1933@hotmail.com>
2025-02-17 00:03:43 -0600

    Install speech

--- AI-assistant.py
+++ AI-assistant.py
@@ -8,8 +8,8 @@ def speak(audio):
     engine.runAndWait()
 
 def time_():
-    Time = datetime.datetime.now().strftime("%I:%M:%S")
-    speak("La hora Actual es")
+    Time = datetime.datetime.now().strftime("%I:%M:%S") # 12 Hour clock
+    speak("La hora actual es")
     speak(Time)
 
 def date_():
@@ -17,9 +17,26 @@ def date_():
     month = datetime.datetime.now().month
     day = datetime.datetime.now().day
     speak("La fecha actual es")
-    speak(day)  
-    speak(month)
-    speak(year)
+    speak(str(day))   # Convertir a cadena
+    speak(str(month)) # Convertir a cadena
+    speak(str(year))  # Convertir a cadena
 
-time_()
-date_()
+def wishme():
+    speak("Bienvenido")
+
+    hour = datetime.datetime.now().hour
+    if hour >= 6 and hour < 12:
+        speak("Buenos días")
+    elif hour >= 12 and hour < 18:
+        speak("Buenas tardes")
+    elif hour >= 18 and hour < 24:
+        speak("Buenas noches")
+    else:
+        speak("Buenas noches")
+
+    time_()
+    date_()
+
+    speak("Estoy aquí para ayudarte con cualquier cosa que necesites. ¿En qué puedo ayudarte?")
+
+wishme()
